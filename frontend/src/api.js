cf52fbd81563cac01b9a9fe348f98f5eb6202af6
Mrigaksh <mrigakshdasani@gmail.com>
2026-05-02 13:34:25 +0530

    added check

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
+console.log(import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
